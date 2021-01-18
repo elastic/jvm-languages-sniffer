78730be30dd32e1044902203a02e714e9a27d80c
Sylvain Wallez <sylvain@elastic.co>
2021-01-18 18:43:58 +0100

    Add "keepMajorMinor" tests

--- sniffer/src/test/java/org/elasticsearch/client/LanguageRuntimeVersionsTest.java
+++ sniffer/src/test/java/org/elasticsearch/client/LanguageRuntimeVersionsTest.java
@@ -22,11 +22,40 @@ package org.elasticsearch.client;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class LanguageRuntimeVersionsTest {
+public class LanguageRuntimeVersionsTest extends Assert {
 
     @Test
     public void testNoLanguages() {
         // We have no dependencies on language libraries in the main project
-        Assert.assertEquals("", LanguageRuntimeVersions.getRuntimeMetadata());
+        assertEquals("", LanguageRuntimeVersions.getRuntimeMetadata());
+    }
+
+    @Test
+    public void testKeepMajorMinor() {
+
+        assertEquals("1.2", LanguageRuntimeVersions.keepMajorMinor("1.2.3"));
+        assertEquals("1.2", LanguageRuntimeVersions.keepMajorMinor("1.2"));
+        assertEquals("1", LanguageRuntimeVersions.keepMajorMinor("1"));
+
+        // Test language-specific representations of non-final versions
+
+        // Kotlin
+        assertEquals("1.4", LanguageRuntimeVersions.keepMajorMinor("1.4.20-M2"));
+        assertEquals("1.3", LanguageRuntimeVersions.keepMajorMinor("1.3.0-rc-198"));
+
+        // Scala
+        assertEquals("2.13", LanguageRuntimeVersions.keepMajorMinor("2.13.0-RC3"));
+        assertEquals("2.13", LanguageRuntimeVersions.keepMajorMinor("2.13.0-M5-6e0cba7"));
+        assertEquals("2.11", LanguageRuntimeVersions.keepMajorMinor("2.11.8-18269ea"));
+
+        // Clojure
+        assertEquals("1.10", LanguageRuntimeVersions.keepMajorMinor("1.10.2-alpha1"));
+
+        // Groovy
+        assertEquals("3.0", LanguageRuntimeVersions.keepMajorMinor("3.0.0-rc-3"));
+
+        // JRuby
+        assertEquals("9.1", LanguageRuntimeVersions.keepMajorMinor("9.1.7.0"));
+        assertEquals("9.0", LanguageRuntimeVersions.keepMajorMinor("9.0.0.0.pre2"));
     }
 }
